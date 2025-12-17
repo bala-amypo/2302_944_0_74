e68ce9ed088ec04d5b36b363f6cd86120a6f086e
bala-amypo <bala@amypo.in>
2025-12-17 05:14:33 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -0,0 +1,35 @@
+package com.example.demo.controller;
+
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entity.Studententity;
+import com.example.demo.services.Studentservice;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
+@RestController
+public class Studentcontroller {
+    @Autowired
+    Studentservice src;
+    @PostMapping("/post")//create
+    public Studententity postdata(@RequestBody Studententity st){
+        return src.savedata(st);
+    }
+    @GetMapping("/get")//read
+    public List<Studententity> getdata(){
+        return src.retdata();
+    }
+    @GetMapping("/getid/{id}")
+    public Studententity getIdVal(@PathVariable int id){
+        return src.id(id);
+    }
+}
--- src/main/java/com/example/demo/repository/StudentRepo.java
+++ src/main/java/com/example/demo/repository/StudentRepo.java
@@ -1,8 +1,8 @@
 package com.example.demo.repository;
-
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.example.classes.entity.Studententity;
+import com.example.demo.entity.Studententity;
 
 @Repository
 public interface  StudentRepo extends JpaRepository<Studententity,Long>{

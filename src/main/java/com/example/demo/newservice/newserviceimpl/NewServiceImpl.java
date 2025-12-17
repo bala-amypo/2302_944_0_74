6c5c8a55793981c16702861c417953b4d3fd0ce4
bala-amypo <bala@amypo.in>
2025-12-17 09:27:39 +0000

    User commit

--- src/main/java/com/example/demo/newservice/newserviceimpl/NewServiceImpl.java
+++ src/main/java/com/example/demo/newservice/newserviceimpl/NewServiceImpl.java
@@ -1,15 +1,20 @@
 package com.example.newservice.impl;
 import java.util.*;
 import org.springframework.stereotype.Service;
-import com.example.demo.entity.StudentValidation;
-import com.example.demo.service.StudentService;
+import com.example.demo.newentity.StudentValidation;
+import com.example.demo.newrepository.NewStudentRepo;
+import com.example.demo.newservice.NewService;
 @Service
 
 public class NewServiceImpl implements NewService {
+    private final NewStudentRepo rep;
+    public NewServiceImpl(NewStudentRepo rep){
+        this.rep=rep;
+    }
 
     @Override
     public StudentValidation savedata(StudentValidation newfile){
-
+            return rep.save(newfile);
     }
 
     @Override

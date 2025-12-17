b04ec3aa5cd34260c6f8a03523d0a1de3fccf0fd
bala-amypo <bala@amypo.in>
2025-12-17 09:17:39 +0000

    User commit

--- src/main/java/com/example/demo/newservice/newserviceimpl/NewServiceImpl.java
+++ src/main/java/com/example/demo/newservice/newserviceimpl/NewServiceImpl.java
@@ -1,7 +1,37 @@
-package com.example.demo.newservice;
+package com.example.classes.newservice.newserviceimpl;
 
-import com.
-public class NewServiceImple implments NewService{
+import com.example.demo.newservice.NewService;
+
+public class NewServiceImpl implements NewService {
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        
+    }
+
+    @Override
+    public boolean equals(Object arg0) {
+        // TODO Auto-generated method stub
+        return super.equals(arg0);
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        // TODO Auto-generated method stub
+        super.finalize();
+    }
+
+    @Override
+    public int hashCode() {
+        // TODO Auto-generated method stub
+        return super.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        return super.toString();
+    }
     
-
+    
 }

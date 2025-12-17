6a3a14d1448875c3a7ea233299925466a60f0a3b
bala-amypo <bala@amypo.in>
2025-12-17 08:12:40 +0000

    User commit

--- src/main/java/com/example/demo/newentity/StudentValidation.java
+++ src/main/java/com/example/demo/newentity/StudentValidation.java
@@ -18,4 +18,37 @@ public class StudentValidation{
     private String email;
 
     
+    public StudentValidation(Long id, @NotBlank(message = "No spaces") String name,
+            @NotBlank(message = "no blank allowed") @Email(message = "invalid format") String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    
+    public StudentValidation() {
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    
 }

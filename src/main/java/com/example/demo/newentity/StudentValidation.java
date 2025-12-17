b255f101b5ea18e498a0ca2fbc2b095c4ecc0407
bala-amypo <bala@amypo.in>
2025-12-17 08:02:43 +0000

    User commit

--- src/main/java/com/example/demo/newentity/StudentValidation.java
+++ src/main/java/com/example/demo/newentity/StudentValidation.java
@@ -1,12 +1,14 @@
 package com.example.demo.newentity;
 
 import jakarta.persistence.Id;
-import jakarta.validation.constraints
+import jakarta.validation.constraints.NotBlank;
 
 public class StudentValidation{
     @Id
     private Long id;
-    @NotBlank
+    @NotBlank(message="No spaces")
     private String name;
+    @NotBlank(message="no blank allowed")
+    @Email(message)
     private String email;
 }
